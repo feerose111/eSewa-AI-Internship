d5c38700d79ba4a9ae46d657ca2e1a8665a58ae5
feerose111 <feerose009@gmail.com>
2025-04-18 21:23:33 +0545

    updates on budgetTracker method

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,27 +1,40 @@
 import json
 import pandas as pd
+import random
+import datetime , pytz
+
+tzNepal = pytz.timezone('Asia/Kathmandu')
 class Tracker:
-    category = ['Food', 'other']
+    category = ['food', 'other']
     transaction = []
-    def __init__(self,budget,expense =0, cataName=None ):
-        self.budget = budget
-        self.expense = expense
-        self.cataName = cataName
+
+    def __init__(self):
+        self.budget = 0
+        self.expense = 0
+        self.cataName = None
 
     def addCategory(self, cataName):
         self.cataName = cataName
         if cataName not in Tracker.category:
             Tracker.category.append(cataName)
 
-    def budgetTracker(self, amount=0):
-        self.budget += amount
+    def addBudget(self, budget):
+        self.budget = budget
+        return self.budget
+
+    def budgetTracker(self, expense):
+        self.expense = expense
         remBudget = self.budget
-        if self.expense > self.budget:
+        id = random.randint(1,100000)
+        date = datetime.datetime.now(tzNepal).strftime("%Y-%m-%d %X%p ")
+        if self.expense > remBudget:
             print('Your expense exceeds your budget !!')
         elif self.expense > 0 :
             remBudget -= self.expense
 
         transaction_data = ({
+                'id':id,
+                'datetime': date,
                 'total budget':self.budget,
                 'category':self.cataName,
                 'categories':Tracker.category,
@@ -37,6 +50,9 @@ class Tracker:
             df = pd.DataFrame(Tracker.transaction)
             df.to_csv("transactions.csv", mode='w', index=False, header=True)
             print("Transaction Successful, Updated your budget !!")
+
+    def showHistory(self):
+        pass
 
 
 
--- main.py
+++ main.py
@@ -5,41 +5,51 @@ def main():
 
     print('\n Add Total Monthly Budget')
     amount = int(input('Enter your monthly budget : '))
-    tracker = Tracker(amount)
-    result = tracker.budgetTracker()
-    print( Tracker.transaction)
-    print('Your Monthly Budget is ', result['total budget'])
-    rembudget = Tracker.transaction[-1]['remaining budget']
-
-    print('-------Expense Tracker--------')
-    print('\n1. Add Expense')
-    print('2. Show Remaining Budget')
-    print('3. Show History')
-    option = int(input('Please choose one of the options :'))
-    try:
-        match option:
-            case 1:
-                result = tracker.budgetTracker()
-                category = str(input(f'Enter Category from {Tracker.category}: '))
-                expense = int(input('Add Expense : '))
-                t2 = Tracker(rembudget, expense, category )
-                tracker.budgetTracker()
+    tracker = Tracker()
+    result = tracker.addBudget(amount)
+    print('Your Monthly Budget is ', amount ,'''result['total budget']''')
 
 
-            case 2:
-                pass
+    while True:
+        print('-------Expense Tracker--------')
+        print('\n1. Add Expense')
+        print('2. Show Remaining Budget')
+        print('3. Show History')
+        print('4. Exit')
+        option = int(input('Please choose one of the options :'))
+        try:
+            match option:
+                case 1:
+                    category = str(input(f'Enter Category from {Tracker.category}: ')).lower()
+                    if category == 'other':
+                        newCat = str(input('Enter a new category to add :')).lower()
+                        tracker.addCategory(newCat)
+                        print("New Category added !")
+                        category = str(input(f'Enter Category from {Tracker.category}: ')).lower()
+                    expense = int(input('Add Expense : '))
 
+                    result = tracker.budgetTracker(expense)
+                    tracker.saveTransaction()
 
-    except ValueError:
-        print("Please enter a valid number")
-    t = Tracker("Food", 500)
-    print(Tracker.category)
+                    tracker.budget = result['remaining budget']
 
-    t.addCategory("Transport")
-    print(Tracker.category)
+                    print('Budget Updated')
 
-    result = t.budgetTracker(10000)
-    print(json.dumps(result, indent=4))
+                case 2:
+                    remBalance = Tracker.transaction[-1]['remaining budget']
+                    print('Your remaining balance is ', remBalance)
 
+                case 3:
+                    pass
+
+                case 4:
+                    print('Exiting the expense tracker. GoodBye!')
+                    break
+
+                case _:
+                    print('Invalid option number!!')
+
+        except ValueError:
+            print("Please enter a valid number")
 if __name__ == "__main__":
     main()
